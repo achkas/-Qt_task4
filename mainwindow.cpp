bf29e5538b63dc7795ae73e64c08a1706729b2c8
Igor Achkas <jigoren@yandex.ru>
2024-10-14 19:04:29 +0700

    исправление 1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,22 +8,23 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
 
-   ui->groupBox->setTitle("Выбор");
-   ui->radioButton->setText("Да");
-   ui->radioButton_2->setText("Нет");
+    ui->groupBox->setTitle("Выбор");
+    ui->radioButton->setText("Да");
+    ui->radioButton->setChecked(true);
+    ui->radioButton_2->setText("Нет");
 
 
-   for (int i = 0; i < 10; ++i)
-   {
-       ui->Mylist->addItem(QString("element %1").arg(i));
-   }
+    for (int i = 0; i < 10; ++i)
+    {
+        ui->Mylist->addItem(QString("element %1").arg(i));
+    }
 
-ui->MyButton->setText("Старт");
-ui->MyButton->setCheckable(1);
+    ui->MyButton->setText("Старт");
+    ui->MyButton->setCheckable(1);
 
-ui->MyprogressBar->setMinimum(0);
-ui->MyprogressBar->setMaximum(10);
-ui->MyprogressBar->setValue(0);
+    ui->MyprogressBar->setMinimum(0);
+    ui->MyprogressBar->setMaximum(10);
+    ui->MyprogressBar->setValue(0);
 }
 
 MainWindow::~MainWindow()
@@ -37,21 +38,20 @@ int val=0;
 
 void MainWindow::on_MyButton_clicked()
 {
+    if(val!=10)
+    {
+        val++;
+        ui->MyprogressBar->setValue(val);
+    }
 
-if(val!=10)
-{
-   val++;
-ui->MyprogressBar->setValue(val);
-}
+    else
+    {
+        val=0;
+        val++;
+        ui->MyprogressBar->setValue(val);
+    }
 
-else
-{
-    val=0;
-    val++;
- ui->MyprogressBar->setValue(val);
-}
-
-   qDebug("Value ");
+    //qDebug("Value ");
 }
 
 
